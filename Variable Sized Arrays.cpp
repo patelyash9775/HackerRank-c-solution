2960fe0d28f85fd67274ca9495ed1cbc80d09aa9
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-07-18 18:54:50 +0530

    Creating vector of a vector and solve the problem

---
+++ Variable Sized Arrays.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define long long int ll;
+
+
+int main() {
+    
+    int n, q;
+    cin >> n >> q;
+
+    vector<vector<int>> a(n);
+
+
+    for (int i = 0; i < n; i++) {
+ 
+        int k;
+        cin >> k;
+
+ 
+        a[i].resize(k);
+        for (int j = 0; j < k; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+
+    for (int l = 0; l < q; l++) {
+        int i, j;
+        cin >> i >> j;
+        cout << a[i][j] << endl;
+    }
+
+    return 0;
+}
